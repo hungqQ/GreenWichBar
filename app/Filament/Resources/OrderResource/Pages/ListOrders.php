8272dd68ef29c222df4b2984db248656d9625607
hung <hungnvgch200466@fpt.edu.vn>
2023-04-24 04:31:54 +0700

    create widget in dashboard

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -21,7 +21,7 @@ class ListOrders extends ListRecords
     protected function getHeaderWidgets(): array
     {
         return[
-            OrderStatsOverview::class,
+            OrderStatsOverview::class
         ];
     }
 }
--- app/Filament/Resources/OrderResource/Widgets/OrderStatsOverview.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStatsOverview.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 
+use App\Models\Bartender;
+use App\Models\Customer;
+use App\Models\Drink;
 use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
@@ -11,7 +14,10 @@ class OrderStatsOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
-            Card::make('All Orders', Order::all()->count()),
+            Card::make('All Order', Order::all()->count()),
+            Card::make('All Customer', Customer::all()->count()),
+            Card::make('All Bartender', Bartender::all()->count()),
+            Card::make('All Drink', Drink::all()->count()),
         ];
     }
 }
